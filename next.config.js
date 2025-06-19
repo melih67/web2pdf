9819b7eb91b06cd58c094fbde3d1def5a8a8448e
ACIKOZ Melih <acikozmelih@gmail.com>
2025-06-19 16:29:39 +0200

    Fix Netlify build: Configure Next.js for puppeteer-core compatibility

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium']
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('puppeteer-core', '@sparticuz/chromium')
+    }
+    return config
+  }
+}
 
 module.exports = nextConfig
